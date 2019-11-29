23667ad97b6bfbe233bfea8834b61c9e39e785d3
Lichkeit <57181190+Lichkeit@users.noreply.github.com>
2019-11-29 23:00:07 +0800

    第二個 移動作業

---
+++ UnityProject/Assets/Sunnyland/artwork/Sprites/Fox.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+
+    [Header("移動速度"), Range(0.1f, 0.9f)]
+    public float speed ;
+
+    public Rigidbody2D rig;
+    private float thrust = 10f;
+    public SpriteRenderer Sr;
+    public bool flipX;
+    float move;
+
+
+    private void Start()
+    {
+
+        rig = GetComponent<Rigidbody2D>();
+       
+
+    }
+
+
+
+    private void Update()
+    {
+        move= Input.GetAxisRaw("Horizontal")*speed;
+        transform.position = new Vector3(transform.position.x+ move, transform.position.y, transform.position.z);
+
+        Vector2 movement = new Vector2(move, move);
+        rig.AddForce(movement  * speed,ForceMode2D.Impulse);
+    }
+
+  
+}
